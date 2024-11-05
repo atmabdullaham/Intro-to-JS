9e65c6f8505a9657d1156b5e3057b5ab81f6cde5
atmabdullaham <atmabdullaham@gmail.com>
2024-11-05 08:45:32 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,88 @@
+/** 
+ * Variable ==> vary + able
+ * 5 things to declare a variable
+ * Variables are Containers for Storing Data
+ * Automatically
+Using var
+Using let
+Using const
+*/
+var weight = 38;
+var price = 81;
+var year = 1995;
+var money = 5005;
+
+
+var age;
+console.log(weight);
+console.log(year)
+
+var age = 17;
+age = 35;
+
+console.log(age)
+
+//_________ Example of using var
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+// console.log(z)
+
+//_________ Example of Using let
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+// console.log(z)
+
+//_________ Example using const
+// const x = 5;
+// const y = 6;
+// const z = x + y;
+// console.log(z)
+
+//___________Mixed Example
+// const price1 = 5;
+// const price2 = 6;
+// let total = price1 + price2;
+
+// console.log(total);
+
+// The two variables price1 and price2 are declared with the const keyword.
+
+// These are constant values and cannot be changed.
+
+// The variable total is declared with the let keyword.
+
+// The value total can be changed.
+
+/**
+ * JavaScript Identifiers
+ *
+ * All JavaScript variables must be identified with unique names.
+ * These unique names are called identifiers
+ *
+ *
+ */
+
+// The general rules for constructing names for variables (unique identifiers) are:
+// Names can contain letters, digits, underscores, and dollar signs.
+
+// Name must begin with a letter.
+
+// Names can also begin with $ and _
+
+// Names are case sensitive
+
+// Reversed words cannot be used as names
+
+// _______________The Assignment Operator___
+
+// In javaScript, the equal sign (=) is an assignment operator, not an "equal to" operator.
+
+//  The "equal to" operator is written like == in JavaScript
+
+// JavaScript variables can hold numbers like 100 and text values like "John Doe"
+
+// In Programming, text values are called text strings.
+
+// 
